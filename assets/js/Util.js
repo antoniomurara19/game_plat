1b726a340d8b5edc790c10adf994a1029311bbfb
antonio murara zabala <antoniomurar4@gmail.com>
2023-08-14 11:16:03 -0300

    gravidade meio estranha e boneco

--- assets/js/Util.js
+++ assets/js/Util.js
@@ -1,4 +1,7 @@
 class Obj {
+    frame = 1
+    tempo = 0
+
     constructor(x,y,w,h,at){
         this.x = x,
         this.y = y,
@@ -13,10 +16,43 @@ class Obj {
         des.drawImage(img,this.x,this.y,this.w,this.h)
     }
 
+    anim(nome){
+        this.tempo += 1
+        if(this.tempo > 12){
+            this.tempo = 0
+            this.frame += 1
+        }
+        if(this.frame > 4){
+            this.frame = 1
+        }
+        this.img = `assets/img/player/Archer/${nome}${this.frame}.png`
+    }
 }
 
-class Arqueiro extends Obj{
-    
+class Player extends Obj{
+    dir_x = 0
+    dir_y = 0
+    pts = 0
+    vidas = 3
+
+    mov(){
+        // EIXO X
+        this.x += this.dir_x
+        console.log(this.x)
+        if(this.x <= -35){
+            this.x = -35
+        }else if(this.x >= 1100){
+            this.x = 1100
+        }
+        // EIXO Y
+        this.y += this.dir_y
+        if(this.y <= -55){
+            this.y = -55
+        }else if (this.y >= 472){
+            this.y = 472
+        }
+        this.dir_y += 0.5
+    }
 }
 
 class Background extends Obj{
@@ -34,5 +70,12 @@ class Background extends Obj{
             this.x = inicio
         }
     }
+}
 
+class Texto{
+    des_texto(text,x,y,cor,font){
+        des.font = font
+        des.fillStyle = cor
+        des.fillText(text,x,y)
+    }
 }
--- assets/js/bio.js
+++ assets/js/bio.js
@@ -1,12 +1,48 @@
 const des = document.getElementById('des').getContext('2d')
 
+// CÉU
 const bg_sky_1 = new Background(0,0,1200,700,'./assets/img/background/sky_3/1.png')
 const bg_sky_2 = new Background(0,0,1200,700,'./assets/img/background/sky_3/2.png')
 const bg_sky_3 = new Background(0,0,1200,700,'./assets/img/background/sky_3/3.png')
 const bg_sky_4 = new Background(0,0,1200,700,'./assets/img/background/sky_3/4.png')
 const bg_sky_5 = new Background(0,0,1200,700,'./assets/img/background/sky_3/5.png')
-const bg_sky_6 = new Background(0,0,1200,700,'./assets/img/background/sky_3/6.png')
 const bg_sky_7 = new Background(0,0,1200,700,'./assets/img/background/sky_3/7.png')
+
+// PLAYER
+const player = new Player(-35,472,128,128,'./assets/img/player/Archer/walking1.png')
+
+// TEXTO
+const vida = new Texto()
+const tiro = new Texto()
+const pontos = new Texto()
+
+// JOGAR
+let jogar = true
+
+//MOVIMENTO
+document.addEventListener('keydown', (e)=>{
+    if(e.key === 'a'){
+        player.dir_x = - 6
+    }else if(e.key === 'd'){
+        player.dir_x = 6
+    }else if(e.key === 's'){
+        player.dir_y = 6
+    }else if(e.key === 'w'){
+        player.dir_y = - 6
+    }
+})
+
+document.addEventListener('keyup', (e)=>{
+    if(e.key === 'a'){
+        player.dir_x = 0
+    }else if(e.key === 'd'){
+        player.dir_x = 0
+    }else if(e.key === 's'){
+        player.dir_y = 0
+    }else if(e.key === 'w'){
+        player.dir_y = 0
+    }
+})
 
 function desenha(){
     bg_sky_1.des_obj()
@@ -14,17 +50,25 @@ function desenha(){
     bg_sky_3.des_obj()
     bg_sky_4.des_obj()
     bg_sky_5.des_obj()
-    bg_sky_6.des_obj()
     bg_sky_7.des_obj()
+
+    player.des_obj()
+
+    vida.des_texto('Vida : ', 10, 30,'black','30px Arial')
+    tiro.des_texto('Munição : ', 500, 30,'black','30px Arial')
+    pontos.des_texto('Pontos : ', 1050, 30,'black','30px Arial')
 }
 
 function atualiza(){
     bg_sky_3.mov(-1000,1200)
     bg_sky_7.mov1(-1200,1200)
+
+    player.anim('walking')
+    player.mov()
 }
 
 function main(){
-    des.clearRect(0,0,1200,700)
+    des.clearRect(0,0,1200,600)
     atualiza()
     desenha()
     requestAnimationFrame(main)
